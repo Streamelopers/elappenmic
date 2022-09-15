2984ac2d10e976d32a7b227164851a164e62685d
Enmanuel Toribio <enmanuel@torib.io>
2022-09-14 22:04:07 -0400

    Added webview for youtube video player

--- src/lib/components/videos/video_detail_screen.dart
+++ src/lib/components/videos/video_detail_screen.dart
@@ -1,13 +1,44 @@
 import 'package:flutter/material.dart';
 import 'package:src/services/models.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'package:flutter/services.dart';
 
-class VideoDetailScreen extends StatelessWidget {
+class VideoDetailScreen extends StatefulWidget {
   final YoutubeVideo video;
 
-  const VideoDetailScreen({super.key, required this.video});
+  VideoDetailScreen({super.key, required this.video});
+  @override
+  State<VideoDetailScreen> createState() =>
+      _VideoDetailScreenState(video: this.video);
+}
+
+class _VideoDetailScreenState extends State<VideoDetailScreen> {
+  YoutubeVideo video;
+  _VideoDetailScreenState({required this.video});
+  @override
+  void initState() {
+    super.initState();
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.landscapeRight,
+      DeviceOrientation.landscapeLeft,
+    ]);
+  }
+
+  @override
+  dispose() {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Text("K lo k"));
+    return Scaffold(
+      body: WebView(
+          initialUrl: "https://www.youtube.com/embed/${video.key}?controls=0",
+          javascriptMode: JavascriptMode.unrestricted),
+    );
   }
 }
