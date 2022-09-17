e03cc0d4be196590cb6b0421678e67c34831c311
Enmanuel Toribio <enmanuel@torib.io>
2022-09-17 13:57:42 -0400

    Playing the radio

--- src/lib/components/audio/audio_screen.dart
+++ src/lib/components/audio/audio_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:just_audio/just_audio.dart';
+import 'package:audio_session/audio_session.dart';
+import 'package:rxdart/rxdart.dart';
 
 class AudioScreen extends StatefulWidget {
   const AudioScreen({Key? key}) : super(key: key);
@@ -7,14 +10,95 @@ class AudioScreen extends StatefulWidget {
   State<AudioScreen> createState() => _AudioScreenState();
 }
 
-class _AudioScreenState extends State<AudioScreen> {
+class _AudioScreenState extends State<AudioScreen> with WidgetsBindingObserver {
+  final AudioPlayer _player = AudioPlayer();
+
+  @override
+  void initState() {
+    super.initState();
+    _init();
+  }
+
+  Future<void> _init() async {
+    // Inform the operating system of our app's audio attributes etc.
+    // We pick a reasonable default for an app that plays speech.
+    final session = await AudioSession.instance;
+    await session.configure(const AudioSessionConfiguration.speech());
+    // Listen to errors during playback.
+    _player.playbackEventStream.listen((event) {},
+        onError: (Object e, StackTrace stackTrace) {
+      print('A stream error occurred: $e');
+    });
+    // Try to load audio from a source and catch any errors.
+    try {
+      // AAC example: https://dl.espressif.com/dl/audio/ff-16b-2c-44100hz.aac
+      await _player.setAudioSource(
+          AudioSource.uri(Uri.parse("https://radio5.domint.net:8222/stream")));
+    } catch (e) {
+      print("Error loading audio source: $e");
+    }
+  }
+
+  @override
+  void dispose() {
+    _player.dispose();
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (state == AppLifecycleState.paused) {
+      // Release the player's resources when not in use. We use "stop" so that
+      // if the app resumes later, it will still remember what position to
+      // resume from.
+      _player.stop();
+    }
+  }
+
+  Widget _playerButton(PlayerState? playerState) {
+    // 1
+    print('im here');
+    print(playerState);
+    final processingState = playerState?.processingState;
+    final playing = playerState?.playing;
+    if (processingState == ProcessingState.loading ||
+        processingState == ProcessingState.buffering) {
+      // 2
+      return Container(
+        margin: EdgeInsets.all(8.0),
+        width: 64.0,
+        height: 64.0,
+        child: CircularProgressIndicator(),
+      );
+    } else if (playing!) {
+      // 4
+      return IconButton(
+        icon: Icon(Icons.pause),
+        iconSize: 64.0,
+        onPressed: _player.pause,
+      );
+    } else {
+      // 3
+      return IconButton(
+        icon: Icon(Icons.play_arrow),
+        iconSize: 64.0,
+        onPressed: _player.play,
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Audio player'),
+      body: Center(
+        child: StreamBuilder<PlayerState>(
+          stream: _player.playerStateStream,
+          builder: (context, snapshot) {
+            final playerState = snapshot.data;
+            return _playerButton(playerState);
+          },
+        ),
       ),
-      body: Center(child: const Text('Audio player')),
     );
   }
 }
